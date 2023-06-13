0ca4d0aec93310f19e9f6b85a1530e1a87c3e306
Gael  cardona <cardonagael11@gmail.com>
2023-06-13 10:28:37 -0600

    se agregaron todos los temas que no me gustan}

---
+++ treceseissopor.py
@@ -0,0 +1,3 @@
+print("elif else if:este tema la verdad que no lo entiendo mis neuronas mueren cada que escucho else")
+print("las normalizaciones:estas cosas ni deberian de existir el mundo seria  mejor y solo fueron creadas para ver arder el mundo ")
+print("Python:En general todo lo que incluya python es un dolor de cabeza incluso hasta imprimir")
